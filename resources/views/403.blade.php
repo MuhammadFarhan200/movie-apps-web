4ab7019385a84a7bbbc38bb061663ce1a6748644
MuhammadFarhan200 <farhan_200@smkassalaambandung.sch.id>
2022-09-09 11:30:57 +0700

    Menambah role

--- resources/views/403.blade.php
+++ resources/views/403.blade.php
@@ -4,18 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>OOPS!!</title>
+    <title>403</title>
+    <link rel="stylesheet" href="assets/css/main/app.css">
+    <link rel="stylesheet" href="assets/css/pages/error.css">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png">
 </head>
 
 <body>
+    <div id="error">
 
-    <center>
-        <h1>:(</h1>
-        <h2>Maaf Anda Tidak Punya Akses Ke Halaman Berikut!!</h2>
-        <a href="{{ url('/home') }}">Silahkan Kembali!</a>
-    </center>
 
+        <div class="error-page container">
+            <div class="col-md-8 col-12 offset-md-2">
+                <div class="text-center">
+                    <img class="img-error" src="assets/images/samples/error-403.svg" alt="Not Found">
+                    <h1 class="error-title">Forbidden</h1>
+                    <p class="fs-5 text-gray-600">Kamu Tidak Punya Akses Ke Halaman Berikut!!</p>
+                    <a href="{{ url('/home') }}" class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 </body>
 
 </html>
